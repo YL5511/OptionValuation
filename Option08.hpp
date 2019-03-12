2cdfab356f868e6a9ec70745dac565572b6ec25b
YL5511 <47186597+YL5511@users.noreply.github.com>
2019-03-12 16:18:58 -0400

    Update Option08.hpp

--- Option08.hpp
+++ Option08.hpp
@@ -4,7 +4,11 @@
 //
 //  Created by Julia on 2018/11/6.
 //  Copyright © 2018年 Julia. All rights reserved.
-//
+/*
+There is multi inheritance. 
+1. The EurOption class and AmeOption  class are sub classes from the Option class by virtual inheritance. By doing so, we can guarantee a single copy of  N and other member of Option class. 
+2. Put and Call are subclass of EurOption and AmeOption
+*/
 
 #ifndef Option07_hpp
 #define Option07_hpp
@@ -19,7 +23,7 @@ private:
 public:
     void SetN(int N_){N=N_;}
     int GetN(){return  N;}
-    virtual double Payoff(double z)=0;
+    virtual double Payoff(double z)=0;  // Payoff is pure virtual function of the Option class. 
 };
 
 class EurOption: virtual public Option
